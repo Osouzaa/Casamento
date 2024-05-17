79cf5e029be38203066b474e0bcebd74ea3a5912
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-05-17 10:05:06 -0300

    feat; remove imports

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,7 @@
 import { Description } from "../../components/Description";
 import { Header } from "../../components/Header";
 import { SaveTheDate } from "../../components/SaveTheDate";
-import * as C from "./style";
+
 
 const About = () => {
   return (
